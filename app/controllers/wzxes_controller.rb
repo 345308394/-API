1c3fd9bcf9746d266ddabca92c64afe84820fd94
wzx <123@example.com>
2017-08-03 19:34:06 +0800

    jingdianyanzheng

--- app/controllers/wzxes_controller.rb
+++ app/controllers/wzxes_controller.rb
@@ -8,7 +8,7 @@ class WzxesController < ApplicationController
   y = params[:y]
   # distance =params[:distance]
   #{distance}
-  data = Wzx.where("pt<->point'(#{x},#{y})'<5").order("pt<->point'(#{x},#{y})'")
+  data = Wzx.where("pt<->point'(#{x},#{y})'<5").order("pt<->point'(#{x},#{y})'").first
     # data1 =(id: data_id)
   render json:data
    
