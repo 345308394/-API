02c4a8139b5fa5a06e44955c1452870f0cf41df3
wzx <123@example.com>
2017-09-04 21:56:31 +0800

    wanshanshuju

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,19 @@
 
  def create
   if (user_params[:name] == ''  || user_params[:password] == '' || user_params[:password_confirm] == '' || user_params[:key] == '')
-    redirect_to '/users/registererror0'
+    render js: "alert('您输入的信息不完全');"
   elsif (user_params[:password] != user_params[:password_confirm])
-   redirect_to '/users/registererror1'
+  render js: "alert('两次输入的密码不相同');"
  elsif 
   user = User.find_by(name: user_params[:name])
    # render json: {register:'Alread_exits_name'}
-   redirect_to '/users/registererror2'
+   #render html: "<strong>该用户名已经存在</strong>".html_safe
+    render js: "alert('该用户名已经存在');"
  elsif     
    @user = User.create(user_params)
    @user.save
    # render json: {register_success:'yes'}
-  
+   render js: "alert('注册成功');"
   end
 end
 
